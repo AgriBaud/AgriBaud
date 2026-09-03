eef71bc1c2bcf957ce67dba6e2fcb3e856479a5c
Baudler <bgbenoufanou@gmail.com>
2026-09-03 17:44:29 +0200

    Nettoyage des scripts temporaires

--- add_app_js.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-let html = fs.readFileSync('carnet.html', 'utf8');
-if (!html.includes('app.js')) {
-    html = html.replace('</body>', '  <script src="app.js"></script>\n</body>');
-    fs.writeFileSync('carnet.html', html);
-}
--- temp_script_check2_0.js
+++
@@ -1,3 +0,0 @@
-
-    tailwind.config = { theme: { extend: { colors: { brand: '#16a34a', bgLight: '#f0fdf4' } } } }
-  
